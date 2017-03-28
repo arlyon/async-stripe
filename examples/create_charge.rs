1aac4121fd9a4094bd190ab9a639ea2f9bcc0877
Kevin Stenerson <kevin@rapiditynetworks.com>
2017-03-28 15:59:26 -0600

    Add a create_charge example

---
+++ examples/create_charge.rs
@@ -0,0 +1,32 @@
+extern crate stripe;
+
+use std::env;
+
+fn main() {
+    // Create a new client
+    let secret_key = env::var("STRIPE_SECRET_KEY").expect("Missing STRIPE_SECRET_KEY in env");
+    let client = stripe::Client::new(&secret_key);
+
+    // Define a card to charge
+    let mut card = stripe::CardParams::default();
+    card.number = "4242424242424242";
+    card.exp_month = "10";
+    card.exp_year = "20";
+
+    // Define the charge
+    let mut params = stripe::ChargeParams::default();
+    params.amount = Some(1000);
+    params.source = Some(stripe::CustomerSource::Card(card));
+
+    // Create the charge
+    let charge = stripe::Charge::create(&client, params).unwrap();
+
+    // Output in a ~prettyprint format
+    println!("Charge {{
+    id: {:?},
+    amount: {:?},
+    created: {:?},
+    status: {:?},
+    ..
+}}", charge.id, charge.amount, charge.created, charge.status);
+}
