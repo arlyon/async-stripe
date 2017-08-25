4651b0e123e83ecbf2208fd07c67375a95d94270
Anna Baldwin <anna.baldwin21@gmail.com>
2017-08-25 11:23:38 -0600

    Add error items for webhooks

--- src/resources/event.rs
+++ src/resources/event.rs
@@ -1,5 +1,5 @@
 use chrono::{Utc};
-use error::Error;
+pub use error::{Error, ErrorCode, ErrorType, RequestError, WebhookError};
 use resources::{Charge, Invoice, Subscription};
 use hmac::{Hmac, Mac, MacResult};
 use serde_json;
@@ -48,14 +48,14 @@ pub enum EventObject {
 pub struct Webhook {}
 
 impl Webhook {
-    pub fn construct_event(payload: String, sig: String, secret: String) -> Result<Event, Error> {
+    pub fn construct_event(payload: String, sig: String, secret: String) -> Result<Event, WebhookError> {
         let mut headers: Vec<String> = sig.split(",").map(|s| s.trim().to_string()).collect();
 
         // Prepare the signed payload
         let ref mut timestamp: Vec<String> = headers[0].split("=").map(|s| s.to_string()).collect();
         let signed_payload = format!("{}{}{}", timestamp[1], ".", payload);
 
-        // Get Stripe signature
+        // Get Stripe signature from header
         let ref mut signature: Vec<String> = headers[1].split("=").map(|s| s.to_string()).collect();
 
         // Compute HMAC with the SHA256 hash function, using endpoing secret as key and signed_payload string as the message
@@ -66,18 +66,20 @@ impl Webhook {
 
         let bytes_signature = MacResult::from_slice(signature[1].as_bytes());
 
+        // Get current timestamp to compare to signature timestamp
         let current = Utc::now().timestamp();
-        let num_timestamp = timestamp[1].parse::<i64>().unwrap();
+        let num_timestamp = timestamp[1].parse::<i64>()
+            .map_err(|err| WebhookError::BadHeader(err))?;
 
-         if bytes_signature != result {
-             // error
-         }
+        if bytes_signature != result {
+            return Err(WebhookError::BadSignature);
+        }
 
         if current - num_timestamp > 300 {
-            // error
+            return Err(WebhookError::BadTimestamp(num_timestamp));
         }
 
         // return Event
-        return serde_json::from_str(&payload).map_err(|err| Error::from(err));
+        return serde_json::from_str(&payload).map_err(|err| WebhookError::BadParse(err));
     }
 }
