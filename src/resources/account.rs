ee39bc32e15bbb318e5c456a047f86ca9b907389
Kevin Stenerson <kstenerson@developers.wyyerd.com>
2019-05-19 00:06:00 -0600

    Fix bugs in spec

--- src/resources/account.rs
+++ src/resources/account.rs
@@ -322,12 +322,12 @@ pub struct TransferSchedule {
     ///
     /// Only shown if `interval` is monthly.
     /// Payouts scheduled between the 29th and 31st of the month are sent on the last day of shorter months.
-    monthly_anchor: u8,
+    monthly_anchor: Option<u8>,
 
     /// The day of the week funds will be paid out, of the style 'monday', 'tuesday', etc.
     ///
     /// Only shown if `interval` is weekly.
-    weekly_anchor: String,
+    weekly_anchor: Option<String>,
 }
 
 #[derive(Clone, Debug, Deserialize, Serialize)]
