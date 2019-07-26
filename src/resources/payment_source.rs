3aa35bda71279830f926510ed7336c60a7cc704c
Anna Baldwin <anna.baldwin@wyyerd.com>
2019-07-26 15:03:57 -0600

    take out commited out stuff

--- src/resources/payment_source.rs
+++ src/resources/payment_source.rs
@@ -19,22 +19,6 @@ pub enum PaymentSourceParams {
     /// Attach an existing source to an existing customer or
     /// create a new customer from an existing source.
     Source(SourceId),
-
-    // // Creates a `Card` payment method from the full card information
-    // // (e.g. card number, expiration, etc).
-    // //
-    // // You usually want to use a `TokenId` received from
-    // // [Stripe Elements](https://stripe.com/docs/stripe-js)
-    // // instead.
-    // Card(CardParams<'a>),
-
-    // // Creates a `BankAccount` payment method from the full bank information
-    // // (e.g. account number, expiration, etc).
-    // //
-    // // You usually want to use a `TokenId` received from
-    // // [Stripe Elements](https://stripe.com/docs/stripe-js)
-    // // instead.
-    // BankAccount(BankAccountParams<'a>),
 }
 
 impl<'de> ::serde::Deserialize<'de> for PaymentSourceParams {
@@ -44,18 +28,6 @@ impl<'de> ::serde::Deserialize<'de> for PaymentSourceParams {
     {
         use serde::de::{Deserialize, Error};
         use serde::private::de::{Content, ContentRefDeserializer};
-
-        // #[derive(Deserialize)]
-        // pub struct Any {}
-
-        // #[derive(Deserialize)]
-        // #[serde(tag = "object", rename_all = "snake_case")]
-        // pub enum PaymentSourceObjectType {
-        //     Card(Any),
-        //     BankAccount(Any),
-        // }
-
-        // Try deserializing the untagged variants first
         let content = <Content<'_> as Deserialize>::deserialize(deserializer)?;
         let deserializer = ContentRefDeserializer::<D::Error>::new(&content);
         if let Ok(ok) = <SourceId as Deserialize>::deserialize(deserializer) {
@@ -66,22 +38,6 @@ impl<'de> ::serde::Deserialize<'de> for PaymentSourceParams {
             return Ok(PaymentSourceParams::Token(ok));
         }
 
-        // // Deserialize just the tag of one of the tagged variants, then deserialize the matching variant
-        // let deserializer = ContentRefDeserializer::<D::Error>::new(&content);
-        // match <PaymentSourceObjectType as Deserialize>::deserialize(deserializer) {
-        //     Ok(PaymentSourceObjectType::Card(_)) => {
-        //         let deserializer = ContentRefDeserializer::<D::Error>::new(&content);
-        //         return <CardParams<'_> as Deserialize>::deserialize(deserializer)
-        //             .map(PaymentSourceParams::Card);
-        //     }
-        //     Ok(PaymentSourceObjectType::BankAccount(_)) => {
-        //         let deserializer = ContentRefDeserializer::<D::Error>::new(&content);
-        //         return <BankAccountParams<'_> as Deserialize>::deserialize(deserializer)
-        //             .map(PaymentSourceParams::BankAccount);
-        //     }
-        //     _ => {}
-        // }
-
         Err(Error::custom("data did not match any variant of enum PaymentSourceParams"))
     }
 }
@@ -91,22 +47,9 @@ impl<'a> ::serde::Serialize for PaymentSourceParams {
     where
         S: ::serde::ser::Serializer,
     {
-        // #[derive(Serialize)]
-        // #[serde(tag = "object", rename_all = "snake_case")]
-        // enum PaymentSourceTagged<'a> {
-        //     Card(&'a CardParams<'a>),
-        //     BankAccount(&'a BankAccountParams<'a>),
-        // }
-
         match self {
             PaymentSourceParams::Source(id) => id.serialize(serializer),
             PaymentSourceParams::Token(id) => id.serialize(serializer),
-            // PaymentSourceParams::Card(card) => {
-            //     PaymentSourceTagged::Card(card).serialize(serializer)
-            // }
-            // PaymentSourceParams::BankAccount(account) => {
-            //     PaymentSourceTagged::BankAccount(account).serialize(serializer)
-            // }
         }
     }
 }
