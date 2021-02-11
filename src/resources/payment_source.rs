2445c13843e1e362aaa3a92fe002a2fe89495fe3
Alexander Lyon <arlyon@me.com>
2021-02-11 10:30:07 +0100

    Unpin serde and remove private api

--- src/resources/payment_source.rs
+++ src/resources/payment_source.rs
@@ -10,7 +10,8 @@ use serde_derive::{Deserialize, Serialize};
 ///
 /// Not to be confused with `SourceParams` which is used by `Source::create`
 /// to create a source that is not necessarily attached to a customer.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Serialize, Deserialize)]
+#[serde(untagged)]
 pub enum PaymentSourceParams {
     /// Creates a payment method (e.g. card or bank account) from tokenized data,
     /// using a token typically received from Stripe Elements.
@@ -21,46 +22,12 @@ pub enum PaymentSourceParams {
     Source(SourceId),
 }
 
-impl<'de> ::serde::Deserialize<'de> for PaymentSourceParams {
-    fn deserialize<D>(deserializer: D) -> Result<Self, D::Error>
-    where
-        D: ::serde::de::Deserializer<'de>,
-    {
-        use serde::de::{Deserialize, Error};
-        use serde::private::de::{Content, ContentRefDeserializer};
-        let content = <Content<'_> as Deserialize>::deserialize(deserializer)?;
-        let deserializer = ContentRefDeserializer::<D::Error>::new(&content);
-        if let Ok(ok) = <SourceId as Deserialize>::deserialize(deserializer) {
-            return Ok(PaymentSourceParams::Source(ok));
-        }
-        let deserializer = ContentRefDeserializer::<D::Error>::new(&content);
-        if let Ok(ok) = <TokenId as Deserialize>::deserialize(deserializer) {
-            return Ok(PaymentSourceParams::Token(ok));
-        }
-
-        Err(Error::custom("data did not match any variant of enum PaymentSourceParams"))
-    }
-}
-
-impl<'a> ::serde::Serialize for PaymentSourceParams {
-    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
-    where
-        S: ::serde::ser::Serializer,
-    {
-        match self {
-            PaymentSourceParams::Source(id) => id.serialize(serializer),
-            PaymentSourceParams::Token(id) => id.serialize(serializer),
-        }
-    }
-}
-
 /// A PaymentSource represents a payment method _associated with a customer or charge_.
 /// This value is usually returned as a subresource on another request.
 ///
 /// Not to be confused with `Source` which represents a "generic" payment method
 /// returned by the `Source::get` (which could still be a credit card, etc)
 /// but is not necessarily attached to either a customer or charge.
-
 #[derive(Clone, Debug, Deserialize, Serialize)]
 #[serde(tag = "object", rename_all = "snake_case")]
 pub enum PaymentSource {
