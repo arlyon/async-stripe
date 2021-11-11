8a030a55c48594689c2e05aee103e785dc72d2c6
Erich Lohrmann <erich.lohrmann.soccer@gmail.com>
2021-11-10 21:27:35 -0500

    Fixed some of the tests that had to change

--- tests/plan_interval.rs
+++ tests/plan_interval.rs
@@ -9,7 +9,7 @@ fn can_create_plan() {
     let id = "price_123".parse().unwrap();
     mock::with_client(|client| {
         let mut plan = stripe::Plan::retrieve(client, &id, &[]).unwrap();
-        plan.interval = Some(stripe::PlanInterval::Month);
+        plan.interval = Box::new(Some(stripe::PlanInterval::Month));
     });
 }
 
@@ -18,7 +18,7 @@ fn can_create_plan() {
 fn can_create_subscription_interval() {
     let recurring = stripe::SubscriptionPriceDataRecurring {
         interval: stripe::SubscriptionInterval::Month,
-        interval_count: Some(100),
+        interval_count: Box::new(Some(100)),
     };
 }
 
@@ -28,17 +28,17 @@ fn can_create_subscription_plan_interval() {
     mock::with_client(|client| {
         let id = "sub_123".parse().unwrap();
         let mut create = stripe::CreateSubscriptionItem::new(id);
-        create.price_data = Some(stripe::SubscriptionItemPriceData {
+        create.price_data = Box::new(Some(stripe::SubscriptionItemPriceData {
             currency: stripe::Currency::USD,
             product: "My Product".to_string(),
             recurring: stripe::SubscriptionItemPriceDataRecurring {
                 interval: stripe::SubscriptionItemInterval::Day,
-                interval_count: Some(6),
+                interval_count: Box::new(Some(6)),
             },
-            tax_behavior: None,
-            unit_amount: None,
-            unit_amount_decimal: None,
-        });
+            tax_behavior: Box::new(None),
+            unit_amount: Box::new(None),
+            unit_amount_decimal: Box::new(None),
+        }));
         let result = stripe::SubscriptionItem::create(client, create).unwrap();
     });
 }
