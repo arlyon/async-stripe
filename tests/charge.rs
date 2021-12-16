bc0b584fa2c14feec190fbeb3a6715386ab98d83
Erich Lohrmann <erich.lohrmann.soccer@gmail.com>
2021-12-15 23:39:26 -0500

    Fixed the tests for runtime blocking

--- tests/charge.rs
+++ tests/charge.rs
@@ -11,11 +11,11 @@ fn is_charge_retrievable() {
             Ok(ok) => ok,
         };
         assert_eq!(charge.id, "ch_123");
-        if let Some(cus) = *charge.customer {
-            assert!(!cus.is_object());
+        if let Some(cus) = charge.customer {
+            assert!(!(*cus).is_object());
         }
-        if let Some(inv) = *charge.invoice {
-            assert!(!inv.is_object());
+        if let Some(inv) = charge.invoice {
+            assert!(!(*inv).is_object());
         }
     });
 }
@@ -46,11 +46,11 @@ fn is_charge_expandable() {
             Ok(ok) => ok,
         };
         assert_eq!(charge.id, "ch_123");
-        if let Some(cus) = *charge.customer {
-            assert!(cus.is_object());
+        if let Some(cus) = charge.customer {
+            assert!((*cus).is_object());
         }
-        if let Some(inv) = *charge.invoice {
-            assert!(inv.is_object());
+        if let Some(inv) = charge.invoice {
+            assert!((*inv).is_object());
         }
     });
 }
