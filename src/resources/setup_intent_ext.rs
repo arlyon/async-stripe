8ee29db27e12ec141d476d1289a7a3b4926bbf06
Erich Lohrmann <erich.lohrmann.soccer@gmail.com>
2021-12-29 18:44:57 -0500

    Added a hand coded setup intent API

--- src/resources/setup_intent_ext.rs
+++ src/resources/setup_intent_ext.rs
@@ -1,14 +1,57 @@
-use crate::{resources::SetupIntent, Expandable, PaymentMethod};
+use serde_derive::Serialize;
 
-impl SetupIntent {
-    pub async fn confirm(&self, payment_method: Option<Expandable<PaymentMethod>>) -> SetupIntent {
-        unimplemented!()
+use crate::config::{Client, Response};
+use crate::resources::SetupIntent;
+use crate::SetupIntentId;
+
+#[derive(Clone, Debug, Serialize)]
+pub struct ConfirmSetupIntent {
+    /// Specifies which payment method
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub payment_method: Option<String>,
+
+    //Mandate data and payment method options not implemented.  If you want
+    //something better, create an issue and lets fix
+    /// Where to redirect the user after they log out of their dashboard.
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub redirect_url: Option<String>,
+}
+
+#[derive(Clone, Debug, Serialize)]
+pub struct CancelSetupIntent {}
+
+pub trait SetupIntentExt {
+    fn confirm(
+        client: &Client,
+        setup_id: &SetupIntentId,
+        params: ConfirmSetupIntent,
+    ) -> Response<Self>
+    where
+        Self: Sized;
+
+    fn cancel(
+        client: &Client,
+        setup_id: &SetupIntentId,
+        params: CancelSetupIntent,
+    ) -> Response<Self>
+    where
+        Self: Sized;
+}
+
+impl SetupIntentExt for SetupIntent {
+    fn confirm(
+        client: &Client,
+        setup_id: &SetupIntentId,
+        params: ConfirmSetupIntent,
+    ) -> Response<SetupIntent> {
+        client.post_form(&format!("/setup_intents/{}/confirm", setup_id), &params)
     }
 
-    pub async fn cancel(
-        &self,
-        cancellation_reason: Option<SetupIntentCancellationReason>,
-    ) -> SetupIntent {
+    fn cancel(
+        _client: &Client,
+        _setup_id: &SetupIntentId,
+        _params: CancelSetupIntent,
+    ) -> Response<SetupIntent> {
         unimplemented!()
     }
 }
