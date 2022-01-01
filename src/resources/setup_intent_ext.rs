470b2ede26459c61c45391fa705fc6df9a4fbf35
Erich Lohrmann <erich.lohrmann.soccer@gmail.com>
2022-01-01 11:02:02 -0500

    Added some debugging code

--- src/resources/setup_intent_ext.rs
+++ src/resources/setup_intent_ext.rs
@@ -65,7 +65,8 @@ impl SetupIntentExt for SetupIntent {
     }
 
     fn retrieve_ext(client: &Client, setup_intent_secret: &String) -> Response<SetupIntent> {
-        client.get(&format!("/setup_intents/{}", setup_intent_secret))
+        unimplemented!()
+        //client.get(&format!("/setup_intents/{}", setup_intent_secret))
     }
 }
 
