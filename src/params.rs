f032570488e601c27f9daf5e616a6360c7e30a44
Alexander Lyon <arlyon@me.com>
2022-04-07 10:20:43 +0100

    fix: use get_query to fix bad query strings

--- src/params.rs
+++ src/params.rs
@@ -217,14 +217,11 @@ impl<T: DeserializeOwned + Send + 'static> List<T> {
     /// Prefer `List::next` when possible
     pub fn get_next(client: &Client, url: &str, last_id: &str) -> Response<List<T>> {
         if url.starts_with("/v1/") {
-            // TODO: Maybe parse the URL?  Perhaps `List` should always parse its `url` field.
-            let mut url = url.trim_start_matches("/v1/").to_string();
-            if url.contains('?') {
-                url.push_str(&format!("&starting_after={}", last_id));
-            } else {
-                url.push_str(&format!("?starting_after={}", last_id));
-            }
-            client.get(&url)
+            let path = url.trim_start_matches("/v1/").to_string(); // the url we get back is prefixed
+            client.get_query(
+                &path,
+                [("starting_after", last_id)].iter().cloned().collect::<HashMap<_, _>>(),
+            )
         } else {
             err(StripeError::UnsupportedVersion)
         }
