a839e60eb0f862af86bbb639d10d21e598563e49
FL33TW00D <fleetwoodpersonal@gmail.com>
2022-06-23 17:34:40 +0100

    chore: fix missing param

--- tests/checkout.rs
+++ tests/checkout.rs
@@ -5,7 +5,7 @@ mod mock;
 fn is_checkout_session_retrievable() {
     mock::with_client(|client| {
         let id = "cs_test_123".parse().unwrap();
-        let result = stripe::CheckoutSession::retrieve(client, &id);
+        let result = stripe::CheckoutSession::retrieve(client, &id, &[]);
         let session = match result {
             Err(err) => panic!("{}", err),
             Ok(ok) => ok,
