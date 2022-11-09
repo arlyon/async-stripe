6ea4ef8f24a5b39e71c201f71a2ee2061e2098c2
Alexander Lyon <arlyon@me.com>
2022-11-09 09:11:55 +0000

    ignore failing test due to stripe-mock

--- tests/checkout.rs
+++ tests/checkout.rs
@@ -1,6 +1,7 @@
 mod mock;
 
 #[test]
+#[ignore] // stripe-mock doesn't include the `created` flag: https://github.com/stripe/stripe-mock/issues/371
 #[cfg(feature = "blocking")]
 fn is_checkout_session_retrievable() {
     mock::with_client(|client| {
