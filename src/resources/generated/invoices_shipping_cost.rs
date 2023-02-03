8e84508357e3af0b14190b35d6cf5906a6cd41ef
Alexander Lyon <arlyon@me.com>
2023-02-03 15:16:48 +0000

    chore: export invoices_shipping_cost

--- src/resources/generated/invoices_shipping_cost.rs
+++ src/resources/generated/invoices_shipping_cost.rs
@@ -2,14 +2,14 @@
 // This file was automatically generated.
 // ======================================
 
-use crate::params::{Expandable};
-use crate::resources::{ShippingRate, TaxRate};
 use serde::{Deserialize, Serialize};
+
+use crate::params::Expandable;
+use crate::resources::{ShippingRate, TaxRate};
 
 /// The resource representing a Stripe "InvoicesShippingCost".
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct InvoicesShippingCost {
-
     /// Total shipping cost before any taxes are applied.
     pub amount_subtotal: i64,
 
@@ -31,7 +31,6 @@ pub struct InvoicesShippingCost {
 
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct LineItemsTaxAmount {
-
     /// Amount of tax applied for this rate.
     pub amount: i64,
 
