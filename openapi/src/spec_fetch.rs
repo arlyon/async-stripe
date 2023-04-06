be0a372d5a2f2481edd305a058a492b150b66ab7
Alexander Lyon <arlyon@me.com>
2023-04-06 11:21:20 +0100

    prevent re-fetch of spec when file hasn't changed

--- openapi/src/spec_fetch.rs
+++ openapi/src/spec_fetch.rs
@@ -60,25 +60,62 @@ fn get_current_openapi_tag() -> anyhow::Result<String> {
 
 pub fn fetch_spec(version: SpecVersion, in_path: &str) -> anyhow::Result<Value> {
     let client = Client::new();
-    let version = match version {
+
+    let desired_version = match version {
         SpecVersion::Latest => get_latest_openapi_tag(&client)?,
         SpecVersion::Current => get_current_openapi_tag()?,
         SpecVersion::Version(v) => v,
     };
-    log::info!("Fetching OpenAPI spec version {}", version);
+
+    log::info!("fetching OpenAPI spec version {}", desired_version);
+
+    if let Some(value) = fs::File::open(in_path)
+        .ok()
+        .and_then(|f| serde_json::from_reader(f).ok())
+        .filter(|value| read_x_stripe_tag(value) == Some(&desired_version))
+    {
+        return Ok(value);
+    }
+
+    if let Ok(file) = fs::File::open(in_path) {
+        if let Ok(value) = serde_json::from_reader(file) {
+            let cached_version = read_x_stripe_tag(&value);
+            if cached_version == Some(&desired_version) {
+                return Ok(value);
+            }
+        }
+    }
 
     let url = format!(
         "https://raw.githubusercontent.com/stripe/openapi/{}/openapi/spec3.sdk.json",
-        version
+        &desired_version
     );
 
-    let spec: Value = client.get(url).send()?.error_for_status()?.json()?;
+    let mut spec: Value = client.get(url).send()?.error_for_status()?.json()?;
+    write_x_stripe_tag(&mut spec, &desired_version)?;
+
     let writer = fs::File::create(in_path)?;
     serde_json::to_writer_pretty(writer, &spec)?;
     log::info!("Wrote OpenAPI spec to {}", in_path);
 
     let version_file_writer = fs::File::create(VERSION_FILE_PATH)?;
-    serde_json::to_writer_pretty(version_file_writer, &VersionFile { version })?;
+    serde_json::to_writer_pretty(version_file_writer, &VersionFile { version: desired_version })?;
 
     Ok(spec)
+}
+
+fn write_x_stripe_tag(spec: &mut Value, version: &str) -> anyhow::Result<()> {
+    spec.as_object_mut()
+        .context("must be an object")?
+        .get_mut("info")
+        .context("must have info field")?
+        .as_object_mut()
+        .context("must be an object")?
+        .insert("x-stripeTag".to_string(), version.into());
+
+    Ok(())
+}
+
+fn read_x_stripe_tag(spec: &Value) -> Option<&str> {
+    spec.as_object()?.get("info")?.as_object()?.get("x-stripeTag")?.as_str()
 }
