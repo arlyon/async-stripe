a59d4c4c468979cd7644a434aa4abf362d5e033b
Alexander Lyon <arlyon@me.com>
2023-04-06 12:17:28 +0100

    add support for raw hyperlinks in the docs

--- src/resources/generated/issuing_card.rs
+++ src/resources/generated/issuing_card.rs
@@ -182,7 +182,7 @@ pub struct IssuingCardShipping {
 pub struct IssuingCardShippingCustoms {
     /// A registration number used for customs in Europe.
     ///
-    /// See https://www.gov.uk/eori and https://ec.europa.eu/taxation_customs/business/customs-procedures-import-and-export/customs-procedures/economic-operators-registration-and-identification-number-eori_en.
+    /// See <https://www.gov.uk/eori> and <https://ec.europa.eu/taxation_customs/business/customs-procedures-import-and-export/customs-procedures/economic-operators-registration-and-identification-number-eori_en>.
     pub eori_number: Option<String>,
 }
 
