8fa70b10ad0af9c2708bfd642b724b4aace56ea6
Chrislearn Young <chris@acroidea.com>
2023-04-18 15:08:32 +0800

    u

--- src/resources/generated/tax_transaction.rs
+++ src/resources/generated/tax_transaction.rs
@@ -65,7 +65,7 @@ impl TaxTransaction {
     /// Creates an item to be added to a draft invoice (up to 250 items per invoice).
     ///
     /// If no invoice is specified, the item will be on the next invoice created for the customer specified.
-    pub fn create(
+    pub fn create_from_calculation(
         client: &Client,
         params: CreateFromTaxCalculation<'_>,
     ) -> Response<TaxTransaction> {
