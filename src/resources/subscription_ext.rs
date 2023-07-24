5b5447230eed54f98d95cdd90138efbac076b2d5
hzargar2 <hzargar2@gmail.com>
2023-07-24 16:17:29 -0400

    added search api functionality for charge, customer, invoice, payment_intent, price, product, and subscription

--- src/resources/subscription_ext.rs
+++ src/resources/subscription_ext.rs
@@ -2,6 +2,7 @@ use serde::Serialize;
 
 use crate::client::{Client, Response};
 use crate::ids::SubscriptionId;
+use crate::params::SearchList;
 use crate::resources::{CreateSubscriptionItems, Subscription};
 
 #[derive(Clone, Debug, Default, Serialize)]
@@ -16,6 +17,22 @@ impl CancelSubscription {
     }
 }
 
+#[derive(Clone, Debug, Default, Serialize)]
+pub struct SubscriptionSearchParams<'a> {
+    pub query: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub limit: Option<u64>,
+    #[serde(skip_serializing_if = "Option::is_none")]
+    pub page: Option<u64>,
+    pub expand: &'a [&'a str],
+}
+
+impl<'a> SubscriptionSearchParams<'a> {
+    pub fn new() -> SubscriptionSearchParams<'a> {
+        SubscriptionSearchParams { query: String::new(), limit: None, page: None, expand: &[] }
+    }
+}
+
 impl Subscription {
     /// Cancels a subscription.
     ///
@@ -26,6 +43,15 @@ impl Subscription {
         params: CancelSubscription,
     ) -> Response<Subscription> {
         client.delete_query(&format!("/subscriptions/{}", subscription_id), params)
+    }
+    /// Searches for a subscription.
+    ///
+    /// For more details see <https://stripe.com/docs/api/subscriptions/search>.
+    pub fn search(
+        client: &Client,
+        params: SubscriptionSearchParams,
+    ) -> Response<SearchList<Subscription>> {
+        client.get_query("/subscriptions/search", params)
     }
 }
 
