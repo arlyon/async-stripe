563e39c573552753132cfd411a8512b1cd6116d9
Ben Brandt <benjamin.j.brandt@gmail.com>
2023-09-27 12:16:35 +0200

    fix: Make subscription_details.metadata optional
    It appears that metadata can be null from the stripe api

--- src/resources/generated/invoice.rs
+++ src/resources/generated/invoice.rs
@@ -670,7 +670,7 @@ pub struct SubscriptionDetailsData {
     /// Set of [key-value pairs](https://stripe.com/docs/api/metadata) that will reflect the metadata of the subscription at the time of invoice creation.
     ///
     /// *Note: This attribute is populated only for invoices created on or after June 29, 2023.*.
-    pub metadata: Metadata,
+    pub metadata: Option<Metadata>,
 }
 
 /// The parameters for `Invoice::create`.
