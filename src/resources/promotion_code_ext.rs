3b4cde6968b0a484679965910ba3b0e214106a28
Lev Khoroshansky <sololev27@gmail.com>
2024-08-11 23:47:30 +0200

    style: Format

--- src/resources/promotion_code_ext.rs
+++ src/resources/promotion_code_ext.rs
@@ -66,10 +66,7 @@ pub struct CreatePromotionCodeRestrictions<'a> {
     /// Each key must be a three-letter [ISO currency code](https://www.iso.org/iso-4217-currency-codes.html) and a [supported currency](https://stripe.com/docs/currencies).
     #[serde(skip_serializing_if = "Option::is_none")]
     pub currency_options: Option<
-        &'a std::collections::HashMap<
-            Currency,
-            CreatePromotionCodeRestrictionsCurrencyOptions,
-        >,
+        &'a std::collections::HashMap<Currency, CreatePromotionCodeRestrictionsCurrencyOptions>,
     >,
     /// A Boolean indicating if the Promotion Code should only be redeemed for Customers without any successful payments or invoices.
     #[serde(skip_serializing_if = "Option::is_none")]
