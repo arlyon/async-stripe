e4e722007a5164413054a958ea13858afbccaafd
Augusto César <augusto.silva@mentimeter.com>
2024-09-24 09:17:11 +0200

    feat: export checkout_session_ext
    ### Description
    Currently, the `checkout_session_ext` is not being exported, which makes the `RetrieveCheckoutSessionLineItems` not being able to be used.
    
    With this change, the following should now be possible:
    ```rust
    use std::str::FromStr;
    
    use stripe::{CheckoutSession, CheckoutSessionId, Client, RetrieveCheckoutSessionLineItems};
    
    #[tokio::main]
    async fn main() {
        let secret_key = std::env::var("STRIPE_SECRET_KEY").expect("Missing STRIPE_SECRET_KEY in env");
        let client = Client::new(secret_key);
    
        let _line_items = CheckoutSession::retrieve_line_items(
            &client,
            &CheckoutSessionId::from_str("").unwrap(),
            &RetrieveCheckoutSessionLineItems::default(),
        )
        .await
        .unwrap();
    }
    ```
    
    Closes #614

--- src/resources.rs
+++ src/resources.rs
@@ -187,6 +187,7 @@ pub use {
 #[rustfmt::skip]
 #[cfg(feature = "checkout")]
 pub use {
+    checkout::checkout_session_ext::*,
     generated::checkout::{
         checkout_session::*,
         payment_link::*,
