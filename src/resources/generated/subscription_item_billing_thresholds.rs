22d0b790417f73d0421cabecd58cfad9e200339c
arlyon <arlyon@users.noreply.github.com>
2025-01-10 00:30:11 +0000

    feat: generate latest changes from OpenApi spec

--- src/resources/generated/subscription_item_billing_thresholds.rs
+++ src/resources/generated/subscription_item_billing_thresholds.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 /// The resource representing a Stripe "SubscriptionItemBillingThresholds".
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct SubscriptionItemBillingThresholds {
+
     /// Usage threshold that triggers the subscription to create an invoice.
     pub usage_gte: Option<i64>,
 }
