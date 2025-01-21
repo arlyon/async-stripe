b7510d3cc145df7cd773ee82ed858c08c9c9076d
Matthew Zeitlin <37011898+mzeitlin11@users.noreply.github.com>
2025-01-20 23:08:39 -0500

    Bump to hyper1 and associated deps

--- async-stripe/src/hyper/connector.rs
+++ async-stripe/src/hyper/connector.rs
@@ -3,8 +3,8 @@ compile_error!("Either default-tls or a rustls feature must be enabled");
 
 #[cfg(feature = "default-tls")]
 mod default_tls {
-    use hyper::client::HttpConnector;
     use hyper_tls::HttpsConnector;
+    use hyper_util::client::legacy::connect::HttpConnector;
     pub fn create() -> HttpsConnector<HttpConnector> {
         hyper_tls::HttpsConnector::new()
     }
@@ -14,14 +14,14 @@ mod default_tls {
 
 #[cfg(all(feature = "__rustls", not(feature = "default-tls")))]
 mod rustls {
-    use hyper::client::HttpConnector;
     use hyper_rustls::HttpsConnector;
     use hyper_rustls::HttpsConnectorBuilder;
+    use hyper_util::client::legacy::connect::HttpConnector;
 
     pub fn create() -> HttpsConnector<HttpConnector> {
         let builder = HttpsConnectorBuilder::new();
         #[cfg(feature = "rustls-tls-native")]
-        let with_roots = builder.with_native_roots();
+        let with_roots = builder.with_native_roots().expect("no valid certs found, you may want to use the rustls-tls-webpki-roots feature instead.");
         #[cfg(feature = "rustls-tls-webpki-roots")]
         let with_roots = builder.with_webpki_roots();
         with_roots.https_or_http().enable_http1().enable_http2().build()
