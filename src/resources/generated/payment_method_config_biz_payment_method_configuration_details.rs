32b17118d740806a8aacedbd17ea196e9a45dd62
arlyon <arlyon@users.noreply.github.com>
2025-04-04 00:31:50 +0000

    feat: generate latest changes from OpenApi spec

--- src/resources/generated/payment_method_config_biz_payment_method_configuration_details.rs
+++ src/resources/generated/payment_method_config_biz_payment_method_configuration_details.rs
@@ -7,6 +7,7 @@ use serde::{Deserialize, Serialize};
 /// The resource representing a Stripe "PaymentMethodConfigBizPaymentMethodConfigurationDetails".
 #[derive(Clone, Debug, Default, Deserialize, Serialize)]
 pub struct PaymentMethodConfigBizPaymentMethodConfigurationDetails {
+
     /// ID of the parent payment method configuration used.
     pub parent: Option<String>,
 }
