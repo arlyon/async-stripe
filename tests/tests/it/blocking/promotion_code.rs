cbe932366d6111d1f5f8fa8601619993211c5c1e
Alexander Lyon <arlyon@me.com>
2025-10-02 09:25:58 +0100

    fix test

--- tests/tests/it/blocking/promotion_code.rs
+++ tests/tests/it/blocking/promotion_code.rs
@@ -1,4 +1,6 @@
-use stripe_product::promotion_code::CreatePromotionCode;
+use stripe_product::promotion_code::{
+    CreatePromotionCode, CreatePromotionCodePromotion, CreatePromotionCodePromotionType,
+};
 
 use super::get_client;
 
@@ -7,6 +9,12 @@ use super::get_client;
 fn create_promotion_code() {
     let client = get_client();
 
-    let result = CreatePromotionCode::new("code").active(true).send_blocking(&client).unwrap();
+    let result = CreatePromotionCode::new(CreatePromotionCodePromotion {
+        coupon: Some("test".to_string()),
+        type_: CreatePromotionCodePromotionType::Coupon,
+    })
+    .active(true)
+    .send_blocking(&client)
+    .unwrap();
     assert!(result.active);
 }
